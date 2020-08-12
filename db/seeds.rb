8a2a00492882e3d181894baa4628a842ffb70511
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-08-12 12:18:13 +0200

    model done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating some restaurants..."
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: CATEGORIES.sample
+    )
+  puts "Restaurant with id #{restaurant.id} was created"
+
+end
+
+puts "Created #{Restaurant.count} restaurants"
+
+puts "Cleaning database..."
+Review.destroy_all
+
+puts "Creating some reviews..."
+10.times do
+  review = Review.create(
+    content: Faker::Restaurant.review,
+    stars: rand(1..5)
+    restaurant: 
+    )
+  puts "Reviews with id #{restaurant.id} was created"
+
+end
+
+puts "Created #{Restaurant.count} restaurants"
